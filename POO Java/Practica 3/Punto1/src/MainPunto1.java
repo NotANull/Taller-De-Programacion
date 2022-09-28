7dbb0c1aa12f8530e39360ca37506ab1e18c1b96
Omar Salum <o.e.salum@gmail.com>
2022-09-27 21:22:46 -0300

    Practicas POO con Java

---
+++ POO Java/Practica 3/Punto1/src/MainPunto1.java
@@ -0,0 +1,29 @@
+import PaqueteLectura.*;
+
+public class MainPunto1 {
+
+    public static void main(String[] args) {
+        
+        GeneradorAleatorio.iniciar();
+        
+        double lado1, lado2, lado3;
+        String colorRelleno, colorLinea;
+        
+        lado1 = GeneradorAleatorio.generarDouble(10)+1; //Random entre 1 y 10
+        System.out.println("Lado 1: " + (int)lado1);
+        lado2 = GeneradorAleatorio.generarDouble(10)+1; //Random entre 1 y 10
+        System.out.println("Lado 2: " + (int)lado2);
+        lado3 = GeneradorAleatorio.generarDouble(10)+1; //Random entre 1 y 10
+        System.out.println("Lado 3: " + (int)lado3);
+        
+        colorRelleno = GeneradorAleatorio.generarString(3); //Hagamos cuenta que se ingresan nombres de colores xD
+        colorLinea = GeneradorAleatorio.generarString(3); //Hagamos cuenta que se ingresan nombres de colores xD
+        
+        Triangulo t = new Triangulo(lado1, lado2, lado3, colorRelleno, colorLinea);
+        
+        System.out.println("Perímetro: " + t.calcularPerimetro());
+        System.out.println("Área: " + t.calcularArea());
+        
+    }
+
+}
